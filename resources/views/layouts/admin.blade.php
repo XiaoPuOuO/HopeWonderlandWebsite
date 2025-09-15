d3e7b37fc61beaef1d013e08aa0142585d819344
DESKTOP-TNAD6A7\User <puchiem08160@gmail.com>
2025-09-15 09:45:09 +0800

    Update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', '管理後台') - HopeWonderland Studio</title>
+    <title>《HopeWonderland Studio》</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
     <!-- Favicon -->
